b24d31a1e2072a36f0863eac9d028081c0932297
1jobam <limm2770@naver.com>
2020-01-17 20:27:18 +0900

    20200117

---
+++ WebContent/js/myscript.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+
+function proc2(){
+	area = document.getElementsByTagName("textarea")[0];
+	
+	r = parseInt(Math.random() * 255);
+	g = parseInt(Math.random() * 255);
+	b = parseInt(Math.random() * 255);
+	
+	area.style.border = '3px solid rgb(' + r + ',' + g + ',' + b + ')'; 
+}
